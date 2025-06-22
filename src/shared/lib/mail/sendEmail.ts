b4e1bc3647f361cd6996dae2a42f8b7e5b5c4727
Norris Oduro <norrisjibril@gmail.com>
2025-06-22 09:05:52 +0200

    feat: suggest mailhog smtp env vars and fix imports and format sendEmail.ts file

--- src/shared/lib/mail/sendEmail.ts
+++ src/shared/lib/mail/sendEmail.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { render } from "@react-email/render";
+import { render } from "@react-email/components";
 
 import { env } from "@/env";
 
@@ -8,17 +8,20 @@ type EmailPayload = {
   to: string;
   subject: string;
   text: string;
-  react?: React.ReactNode;
-}
+  react?: React.ReactElement;
+};
 
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: env.SMTP_PORT,
   secure: env.SMTP_SECURE,
-  auth: (env.SMTP_USER && env.SMTP_PASS) ? {
-    user: env.SMTP_USER,
-    pass: env.SMTP_PASS,
-  } : undefined,
+  auth:
+    env.SMTP_USER && env.SMTP_PASS
+      ? {
+          user: env.SMTP_USER,
+          pass: env.SMTP_PASS,
+        }
+      : undefined,
 });
 
 export const sendEmail = async ({ from, to, subject, text, react }: EmailPayload) => {

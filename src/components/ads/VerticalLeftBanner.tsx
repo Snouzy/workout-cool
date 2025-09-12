36dc598ff2c1b4121c75eff866cd57a6cdc934bf
Mathias <mathiasnouzy@gmail.com>
2025-09-12 14:44:00 +0200

    feat(ads): add Ezoic ad component and integrate it into vertical ad banners for improved ad management

--- src/components/ads/VerticalLeftBanner.tsx
+++ src/components/ads/VerticalLeftBanner.tsx
@@ -3,9 +3,18 @@ import { env } from "@/env";
 import { VerticalAdBanner } from "./VerticalAdBanner";
 
 export function VerticalLeftBanner() {
-  if (!env.NEXT_PUBLIC_VERTICAL_LEFT_BANNER_AD_SLOT) {
+  const hasAdSlot = env.NEXT_PUBLIC_VERTICAL_LEFT_BANNER_AD_SLOT;
+  const hasEzoicPlacement = env.NEXT_PUBLIC_EZOIC_VERTICAL_LEFT_PLACEMENT_ID;
+
+  if (!hasAdSlot && !hasEzoicPlacement) {
     return null;
   }
 
-  return <VerticalAdBanner adSlot={env.NEXT_PUBLIC_VERTICAL_LEFT_BANNER_AD_SLOT} position="left" />;
+  return (
+    <VerticalAdBanner
+      adSlot={env.NEXT_PUBLIC_VERTICAL_LEFT_BANNER_AD_SLOT || ""}
+      ezoicPlacementId={env.NEXT_PUBLIC_EZOIC_VERTICAL_LEFT_PLACEMENT_ID}
+      position="left"
+    />
+  );
 }
--- src/components/ads/VerticalRightBanner.tsx
+++ src/components/ads/VerticalRightBanner.tsx
@@ -3,9 +3,18 @@ import { env } from "@/env";
 import { VerticalAdBanner } from "./VerticalAdBanner";
 
 export function VerticalRightBanner() {
-  if (!env.NEXT_PUBLIC_VERTICAL_RIGHT_BANNER_AD_SLOT) {
+  const hasAdSlot = env.NEXT_PUBLIC_VERTICAL_RIGHT_BANNER_AD_SLOT;
+  const hasEzoicPlacement = env.NEXT_PUBLIC_EZOIC_VERTICAL_RIGHT_PLACEMENT_ID;
+  
+  if (!hasAdSlot && !hasEzoicPlacement) {
     return null;
   }
 
-  return <VerticalAdBanner adSlot={env.NEXT_PUBLIC_VERTICAL_RIGHT_BANNER_AD_SLOT} position="right" />;
+  return (
+    <VerticalAdBanner
+      adSlot={env.NEXT_PUBLIC_VERTICAL_RIGHT_BANNER_AD_SLOT || ""}
+      ezoicPlacementId={env.NEXT_PUBLIC_EZOIC_VERTICAL_RIGHT_PLACEMENT_ID}
+      position="right"
+    />
+  );
 }

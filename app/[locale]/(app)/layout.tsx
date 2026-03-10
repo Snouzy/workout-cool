0a4d1504553f6bea71e78522b93268f19afc3b54
Mathias <mathiasnouzy@gmail.com>
2026-03-10 23:13:23 +0100

    fix(layout.tsx, globals.css): adjust layout styles to prevent ad scripts from blocking scroll in the main card

--- app/[locale]/(app)/layout.tsx
+++ app/[locale]/(app)/layout.tsx
@@ -13,7 +13,8 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <div className="card w-full max-w-3xl min-h-[500px] max-h-[90vh] bg-white dark:bg-[#232324] shadow-xl border border-base-200 dark:border-slate-700 flex flex-col justify-between overflow-hidden max-sm:rounded-none max-sm:h-full rounded-lg">
       <Header />
-      <div className="flex-1 overflow-auto flex flex-col">{children}</div>
+      {/* TODO: remove min-h-0 if something is broken */}
+      <div className="flex-1 overflow-auto flex flex-col min-h-0">{children}</div>
       <BottomNavigation />
       <Footer />
     </div>
